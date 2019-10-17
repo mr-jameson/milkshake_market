b4e515b2365de65dda9e9afcc9d7b35fa5026d02
mr-jameson <elliot.jameson4@gmail.com>
2019-10-17 14:43:51 +1100

    added counts to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
-    def Home
+    def home
+        # @ingredients = Ingredient.where(kind: "flavour").or(Ingredient.where(kind: "topping")
+        @flavour_count = Ingredient.flavour.count
+        @topping_count = Ingredient.topping.count
     end
 end
